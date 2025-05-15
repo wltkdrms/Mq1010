1c978208050e0a7f3dd8615b114cd3da9dee1e46
iamoverthemoon <hemisapinweha@gmail.com>
2025-05-15 17:44:57 +1000

    Update Equipment.java

--- Game/Equipment.java
+++ Game/Equipment.java
@@ -1,8 +1,8 @@
 public class Equipment {
     public String name;
     public String rarity;
-    public int bonusAttack; //some equipment gives character more attack power
-    public int bonusDefense; //some equipment gives character more defense power
+    public int bonusAttack;
+    public int bonusDefense;
     public StatusEffect statusEffect;
 
     public Equipment(String name, String rarity, int bonusAttack, int bonusDefense, StatusEffect statusEffect) {
@@ -13,22 +13,19 @@ public class Equipment {
         this.statusEffect = statusEffect;
     }
 
-    //Returns the total attack boost provided by the equipment.
     public int getTotalAttackBoost(){
-        return bonusAttack; 
+        return bonusAttack; //Returns the total attack boost provided by the equipment.
     }
 
-    //Returns the total defense boost provided by the equipment.
     public int getTotalDefenseBoost(){
-        return bonusDefense; 
+        return bonusDefense; //Returns the total defense boost provided by the equipment.
     }
-
-    //Checks whether the equipment has a status effect.
-    public boolean hasStatusEffect(){
+    
+    public boolean hasStatusEffect(){ //Checks whether the equipment has a status effect.
         if (statusEffect != null) {
-            return true; 
+            return true; //TRUE if a status effect is present
         }
-        return false;
+        return false; //FLASE if not present
     }
 }
 
