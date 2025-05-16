d52aa3629b2a5090b7d216553f4b32750553e48b
iamoverthemoon <hemisapinweha@gmail.com>
2025-05-16 20:42:45 +1000

    Update App.java

--- majorAssignmentTopic1/App.java
+++ majorAssignmentTopic1/App.java
@@ -1,5 +1,6 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.startGame(); // This is the method that start off the whole game
     }
 }
