63320f756063a86bb4b74965459b87e2c8dd914e
iamoverthemoon <hemisapinweha@gmail.com>
2025-05-17 17:53:57 +1000

    Update App.java

--- majorAssignmentTopic1/App.java
+++ majorAssignmentTopic1/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) {
         Game game = new Game();
-        game.startGame(); // This is the method that start off the whole game
+        game.startGame(); // This is the method that starts off the whole game
+        game.saveBattleLogToCSV("battle_log.csv"); // This is the method to save the game in to .csv file
     }
 }
