a7061775b4911434e8fecd1e848b2d839e9cd510
chicka-debug <rishika2407@gmail.com>
2025-05-17 18:05:26 +1000

    Update comments BattleLogEntry.java

--- majorAssignmentTopic1/BattleLogEntry.java
+++ majorAssignmentTopic1/BattleLogEntry.java
@@ -1,16 +1,16 @@
 public class BattleLogEntry {
-    public String message;
-    public BattleLogEntry nextEntry;
-
+    public String message; // The battle message for this log entry
+    public BattleLogEntry nextEntry; // Reference to the next log entry
+    
     public BattleLogEntry(String message) {
         this.message = message;
-        this.nextEntry = null;
+        this.nextEntry = null; //// Initially, there is no next entry
     }
 
     public String getMessage() {
         return message;
     }
-
+    //Links this entry to the next one in the log
     public void setNextEntry(BattleLogEntry nextEntry) {
         this.nextEntry = nextEntry;
     }
@@ -19,11 +19,15 @@ public class BattleLogEntry {
         return nextEntry;
     }
 
-    // Recursively print log
+    /*
+     * Recursively prints all log messages starting from this entry.
+     * This method traverses the log using the linked `nextEntry` references,
+     * printing each message until it reaches the end.
+     */
     public void printLog() {
         System.out.println(message);
         if (nextEntry != null) {
-            nextEntry.printLog();  // Recursive call!
+            nextEntry.printLog();  // Recursive call to print the rest of the log
         }
     }
 }
