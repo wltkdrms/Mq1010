1c375900c45ec57b708158391371f216f32e9d25
chicka-debug <rishika2407@gmail.com>
2025-05-17 20:28:31 +1000

    Create Main.java

---
+++ majorAssignmentTopic1/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+public static void main(String[] args) {
+        Game game = new Game();      // Create the game instance
+        game.startGame();            // Start game (sets up teams, picks player, runs loop)  
+    }
+    
+}
