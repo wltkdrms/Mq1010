c095b55721027f591163d82c91478ba0f046ad32
iamoverthemoon <hemisapinweha@gmail.com>
2025-05-19 20:28:45 +1000

    Update App.java

--- majorAssignmentTopic1/App.java
+++ majorAssignmentTopic1/App.java
@@ -2,6 +2,11 @@ public class App {
     public static void main(String[] args) {
         Game game = new Game();
         game.startGame(); // This is the method that starts off the whole game
-        game.saveBattleLogToCSV("battle_log.csv"); // This is the method to save the game in to .csv file
+        // This is the method to save the game in to .csv file
+        try {
+            game.saveBattleLogToCSV("battle_log.csv");
+        } catch (IOException e) {
+            System.out.println("Error writing to file: " + e.getMessage());
+        }
     }
 }
