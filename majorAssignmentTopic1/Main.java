dff3b18de0a1f0ca4bfa0814c69fcf20562e3315
iamoverthemoon <hemisapinweha@gmail.com>
2025-05-19 20:33:53 +1000

    Update Main.java

--- majorAssignmentTopic1/Main.java
+++ majorAssignmentTopic1/Main.java
@@ -3,5 +3,4 @@ public static void main(String[] args) {
         Game game = new Game();      // Create the game instance
         game.startGame();            // Start game (sets up teams, picks player, runs loop)  
     }
-    
 }
