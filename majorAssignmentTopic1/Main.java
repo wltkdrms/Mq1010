2d4546a9d7b0a3e2fbcfb93bab0e81c3c4dbf7cf
chicka-debug <rishika2407@gmail.com>
2025-05-22 18:26:54 +1000

    Delete majorAssignmentTopic1/Main.java

--- majorAssignmentTopic1/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-public static void main(String[] args) {
-        Game game = new Game();      // Create the game instance
-        game.startGame();            // Start game (sets up teams, picks player, runs loop)  
-    }
-}
